a0cd2caa29d2bfe39c1c2970a4a00a3503a9efb6
Lucas Angeli <jl.angeli@outlook.com>
2023-05-19 23:14:56 -0300

    Update loginSelection.jsx

--- src/pages/loginSelection.jsx
+++ src/pages/loginSelection.jsx
@@ -1,14 +1,13 @@
 import Button from "../components/Button";
-import { useRouter } from "next/router"
+import Router from "next/router"
 
 export default function LoginSelectionPage() {
-    const router = useRouter()
     return (
         <div className={`
         bg-gradient-to-b from-black via-gray-900 to-gray-800 w-screen h-screen
             flex flex-col gap-1 justify-center items-center
         `}>
-            <Button className={`bg-white w-[300px] p-3 m-2 rounded-md  font-bold border-0`} onClick={() => router.push('/login')}>
+            <Button className={`bg-white w-[300px] p-3 m-2 rounded-md  font-bold border-0`} onClick={() => Router.push('/login')}>
                 Email / CNPJ
             </Button>
             <Button disabled={true} className={`bg-gray-500 w-[300px] p-3 m-2 rounded-md  font-bold`}>
